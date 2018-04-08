bbb2800bcf3ece78ec3ad4837a49806ac420c1eb
Jeremy Nagahama <naga@hawaii.edu>
2018-04-08 09:41:50 -1000

    Initial setup for /map (titled /test for now)

---
+++ app/imports/ui/pages/Test.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class componentName extends Component {
+    render() {
+        return (
+            <div>
+                <p>all i ever wanted was a roley</p>
+            </div>
+        );
+    }
+}
