9ab4c6b6cd06c6a537fe2d9d3b8b384d5df254db
Jeremy Nagahama <naga@hawaii.edu>
2018-04-08 22:42:13 -1000

    Test on branches

--- app/imports/api/stuff/stuff.js
+++ app/imports/api/stuff/stuff.js
@@ -2,6 +2,8 @@ import { Mongo } from 'meteor/mongo';
 import SimpleSchema from 'simpl-schema';
 import { Tracker } from 'meteor/tracker';
 
+// Ok 
+
 /** Create a Meteor collection. */
 const Stuffs = new Mongo.Collection('Stuffs');
 
