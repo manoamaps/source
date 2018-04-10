eff80ccbbb46c9e8a3e24cbd2f756cd711edf35f
Jeremy Nagahama <naga@hawaii.edu>
2018-04-09 17:00:48 -1000

    Add Map, and set CSS such that maps overlap each other.
    We need to link the value to our maps so that images will show/hide depending
    on the day. example: map1 shows when its night, map2 shows when its day,
    etc.I'm thinking jQuery

--- app/imports/ui/pages/Test.jsx
+++ app/imports/ui/pages/Test.jsx
@@ -5,8 +5,8 @@ import 'react-input-range/lib/css/index.css';
  /*
 
 
-    map1: Faculty Pass
-    map2: Student Pass
+    map1: Normal Student Parking
+    map2: Everywhere Parking
     map3: Night Pass
  */
 export default class Test extends Component {
@@ -16,17 +16,28 @@ export default class Test extends Component {
         this.state = {
             value: 0,
             time: {
-                0: '12:00AM', 1: '12:30AM', 2: '1:00AM', 3: '1:30AM',
-                4: '2:00AM', 5: '2:30AM', 6: '3:00AM',
+                0: '12:00AM', 1: '1:00AM', 2: '2:00AM', 3: '3:00AM',
+                4: '4:00AM', 5: '5:00AM', 6: '6:00AM', 7: '7:00AM',
+                8: '8:00AM', 9: '9:00AM', 10: '10:00AM', 11: '11:00AM',
+                12: '12:00PM', 13: '1:00PM', 14: '2:00PM', 15: '3:00PM',
+                16: '4:00PM', 17: '5:00 PM', 18: '6:00PM', 19: '7:00PM',
+                20: '8:00PM', 21: '9:00PM', 22: '10:00PM', 23: '11:00PM',
+
             },
         };
 
     }
+    // TODO: I don't think we need this.
     returnTime(key) {
         return (this.state.time[key]);
     }
 
     render() {
+        // const containerStyle = {
+        //     position: 'relative',
+        // const imageStyle = {
+        //     position: 'absoulte',
+        // };
         return (
             <div>
                 <Grid columns={2}>
@@ -36,9 +47,13 @@ export default class Test extends Component {
                     </Grid.Column>
                     <Grid.Column>
                         {/* TODO: Set min width/height of map so it doesnt mess up when screen gets small.. */}
-                        <Image className="map" size="huge" bordered src="/images/map.gif"/>
+                        <div className="map-container">
+                        <Image size="huge" src="/images/map.gif"/>
+                        <Image className="map-image" size="huge" src="/images/map1.gif"/>
+                        <Image className="map-image" size="huge" src="/images/map2.gif"/>
+                        </div>
                         <InputRange
-                            maxValue = {48}
+                            maxValue = {24}
                             minValue = {0}
                             value = {this.state.value}
                             onChange = {
