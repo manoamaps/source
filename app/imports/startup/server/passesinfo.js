d110491e72a09a240c8b62187f6bea057fceedcf
andku23 <akurano@hawaii.edu>
2018-04-11 15:36:52 -1000

    You Add passes correctly
    passes have all their correct data now

--- app/imports/startup/server/passesinfo.js
+++ app/imports/startup/server/passesinfo.js
@@ -12,21 +12,24 @@ function addData(data) {
 if (PassesInfo.find().count() === 0) {
   if (Meteor.settings.defaultData) {
     console.log('Creating default data.');
-      PassesInfo.insert({name: "Student", parkingTimes: [4], id: 0});
+    PassesInfo.insert({name: "Student", parkingTimes: {upperCampus: [0, 6, 16, 24], structure: [0, 24], hawaiian: [-1, -1], astronomy: [0, 6, 16, 24]}, id: 0});
+    PassesInfo.insert({name: "Evening", parkingTimes: {upperCampus: [0, 6, 16, 24], structure: [13.5, 24], hawaiian: [-1, -1], astronomy: [0, 6, 16, 24]}, id: 1});
+    PassesInfo.insert({name: "Hawaiian", parkingTimes: {upperCampus: [0, 6, 16, 24], structure: [13.5, 24], hawaiian: [0, 24], astronomy: [0, 6, 16, 24]}, id: 2});
+    PassesInfo.insert({name: "Astronomy", parkingTimes: {upperCampus: [0, 6, 16, 24], structure: [13.5, 24], hawaiian: [-1, -1], astronomy: [0, 24]}, id: 3});
     //Meteor.settings.defaultData.map(data => addData(data));
   }
 }
 
 /** This subscription publishes only the documents associated with the logged in user */
 Meteor.publish('PassesInfo', function publish() {
-  if (this.userId) {
+  /*if (this.userId) {
     const username = Meteor.users.findOne(this.userId).username;
     return PassesInfo.find({ name: username });
-  }
+  }*/
   return this.ready();
 });
 
-/** This subscription publishes all documents regardless of user, but only if the logged in user is the Admin. */
+ //This subscription publishes all documents regardless of user, but only if the logged in user is the Admin. */
 Meteor.publish('PassAdmin', function publish() {
   if (this.userId && Roles.userIsInRole(this.userId, 'admin')) {
     return Passesinfo.find();
